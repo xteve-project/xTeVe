63d0c698290fa773f84f0475ddc8feeb2d23eeb1
martin <martin@villyson.com>
2021-02-26 14:04:33 +0100

    fix new dockerfile

---
+++ docker-root/defaults/grab_xml_tv_se.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Running: tv_grab_eu_xmltvse"
+echo "Configured days: ${XMLTV_DAYS}"
+/usr/local/bin/tv_grab_eu_xmltvse --config-file /config/.xmltv/tv_grab_eu_xmltvse.conf --output /config/tv_grab_eu_xmltvse_guide.xml --days $XMLTV_DAYS
+echo "grabber finished, exiting..."
+exit 0
