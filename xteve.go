e6fdd3a6cd60bc1a261a4254421d6d183e8f1d1c
SCP002 <feedusfeedme@gmail.com>
2022-03-27 22:49:01 +0300

    Increase program version to "2.2.2.0200"

--- xteve.go
+++ xteve.go
@@ -39,7 +39,7 @@ var GitHub = GitHubStruct{Branch: "master", User: "SCP002", Repo: "xTeVe", Updat
 const Name = "xTeVe"
 
 // Version : Version, the Build Number is parsed in the main func
-const Version = "2.2.2.0100"
+const Version = "2.2.2.0200"
 
 // DBVersion : Database Version
 const DBVersion = "2.1.1"
