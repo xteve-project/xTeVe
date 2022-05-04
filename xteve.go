a708be072eecd2a3655a04668f35b494fe4d7c98
SCP002 <feedusfeedme@gmail.com>
2022-05-04 23:21:17 +0300

    Bump program version to 2.5.0, database version to 2.3.0

--- xteve.go
+++ xteve.go
@@ -37,10 +37,10 @@ var GitHub = GitHubStruct{Branch: "master", User: "SCP002", Repo: "xTeVe", Updat
 const Name = "xTeVe"
 
 // Version : Version, the Build Number is parsed in the main func
-const Version = "2.4.1.0000"
+const Version = "2.5.0.0000"
 
 // DBVersion : Database Version
-const DBVersion = "2.2.3"
+const DBVersion = "2.3.0"
 
 // APIVersion : API Version
 const APIVersion = "1.1.0"
